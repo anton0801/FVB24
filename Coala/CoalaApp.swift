97b7330e31c04ec1b63fcc9ae8456016590622e2
Anton <danilovanton724@gmail.com>
2024-11-10 16:59:42 +0100

    Updated

--- Coala/CoalaApp.swift
+++ Coala/CoalaApp.swift
@@ -2,9 +2,24 @@ import SwiftUI
 
 @main
 struct CoalaApp: App {
+    
+    @UIApplicationDelegateAdaptor(CoalaAppDelegate.self) var coalaAppDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+    
+}
+
+
+class CoalaAppDelegate: NSObject, UIApplicationDelegate {
+    
+    static var orientationLock = UIInterfaceOrientationMask.landscape
+    
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return CoalaAppDelegate.orientationLock
+    }
+    
 }
